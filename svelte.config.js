87a91dcf5b76826f0d54bc8da0b90d1d9ac9645b
kenny-io <ekeneeze3@gmail.com>
2021-09-02 16:49:13 +0400

    don't prerender

--- svelte.config.js
+++ svelte.config.js
@@ -8,12 +8,12 @@ const config = {
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
     adapter: adapter(),
-    prerender: {
-      crawl: true,
-      enabled: true,
-      onError: 'continue',
-      pages: ['*'],
-    },
+    // prerender: {
+    //   crawl: true,
+    //   enabled: true,
+    //   onError: 'continue',
+    //   pages: ['*'],
+    // },
   },
 
   preprocess: [
